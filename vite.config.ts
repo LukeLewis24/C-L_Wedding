b31ae0d98abc722c27fe05e70fd79efbc4fb4d61
LukeLewis24 <133044172+LukeLewis24@users.noreply.github.com>
2026-06-16 12:18:51 +0100

    vite routplugin change to tanstacxk

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { routerPlugin } from "@tanstack/router-plugin/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    routerPlugin(),
+    TanStackRouterVite(),
   ],
 });
