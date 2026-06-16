f6642aa8a05724448e37f37d7a1534e377954da3
LukeLewis24 <133044172+LukeLewis24@users.noreply.github.com>
2026-06-16 12:59:06 +0100

    pdfasset to pdfurl

--- src/routes/invitation.tsx
+++ src/routes/invitation.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
-import pdfAsset from "@/assets/wedding-invite.pdf";
+import pdfUrl from "@/assets/wedding-invite.pdf";
 import botanical from "@/assets/botanical-bg.jpg";
 
 export const Route = createFileRoute("/invitation")({
